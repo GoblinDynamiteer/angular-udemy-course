67a99f799b43d0460a70edddb1d3fb8bb72bf643
Johan Kämpe <kampe.johan@gmail.com>
2018-09-15 00:11:00 +0200

    use two-way binding using ngModel
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = false;
-  serverName = '';
+  serverName = 'DefaultServerName';
 
   constructor() {
     setTimeout(() => {this.allowNewServer = true; }, 2000);
